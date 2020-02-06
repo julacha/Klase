5fcf419db73da530f136dfc5f03a78974d22696e
Julia.Zorina <julia.zorinaa@gmail.com>
2020-02-06 12:21:03 +0200

    add changes

--- scripts/avg.js
+++ scripts/avg.js
@@ -1,17 +1,43 @@
 ///Пример: Случайное целое между min и max
 //использование Math.round() даст неравномерное распределение!
 function randomRange(myMin, myMax) {
-    return Math.floor(Math.random() * (myMax - myMin + 1)) + myMin;
-  } 
-function getAverage(numThrows) {
-let total = 0;
-for (let i=0; i < numThrows; i++) {
-  total +-randomRange(1,6); 
+    return Math.floor(Math.random() * (myMax - myMin + 1)) + myMin; // Change this line
 }
- return total / numThrows;
-} 
-const numThrows = 1000;
+
+function getAverage(numThrows) {
+    //write function to get an average value from dice throws
+    let total = 0;
+    //write loop here
+    // use randomRange(1, 6);
+    //loop goes here
+    for (let i = 0; i < numThrows; i++) {
+        total += randomRange(1, 6);
+    }
+
+    return total / numThrows;
+}
+
+const numThrows = 100000;
 let result = getAverage(numThrows);
 
+// 4 ways of selecting a single button
+//
+
 let myParagraph = document.querySelector("#answer");
-myParagraph.textContent = `Average throw value is ${result}`;
+// let myParagraph = document.querySelector("p");
+myParagraph.myResult = result;
+myParagraph.textContent = `Average throw value is ${myParagraph.myResult}`;
+
+const myButton = documment.querySelector(".pretty-button");
+myButton.classList.add("big-button");
+
+const myInput = document.querySelector(".num-input");
+console.log(`My value is ${myInput.value}`);
+
+function onButtonClick(){
+console.log ("Button was clicked");
+console.log ()
+}
+
+myButton.addEventListener('click',() => console.log ("Vliked me!"));
+//document.addEventListener ("click",() => console.log ("Whole Doc clicked"));
