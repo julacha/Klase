3a62f83b69f0a951e34aac6964d0224a6fa87742
Julia.Zorina <julia.zorinaa@gmail.com>
2020-02-06 13:10:56 +0200

    add value

--- scripts/avg.js
+++ scripts/avg.js
@@ -1,22 +1,8 @@
 ///Пример: Случайное целое между min и max
 //использование Math.round() даст неравномерное распределение!
-console.log("Starting work", 6 ** 2);
-let a = 1000; //global a
-//giving a default value to function argument
-function printStuff(a = 50) {
-    console.log("Trying to print a", a);
-    let b = a + 5;
-    let c = a + 6;
-
-    console.log(`Hello Valdis ${a * 82} and there more to know about ${b}`);
-    console.log(`Hello Liga`, b * 2);
-    console.log(`Hello Peter Mr.` + c + " " + b);
-}
-
 function randomRange(myMin, myMax) {
     return Math.floor(Math.random() * (myMax - myMin + 1)) + myMin; // Change this line
 }
-
 function getAverage(numThrows) {
     //write function to get an average value from dice throws
     let total = 0;
@@ -29,11 +15,9 @@ function getAverage(numThrows) {
 
     return total / numThrows;
 }
-
+console.log("starting");
 const numThrows = 100000;
 let result = getAverage(numThrows);
-
-console.log("Going to print answer");
 
 let myParagraph = document.querySelector("#answer");
 // let myParagraph = document.querySelector("p");
@@ -44,6 +28,7 @@ myParagraph.textContent = `Average throw value is ${myParagraph.myResult}`;
 // const myButton = document.querySelector(".pretty-button");
 // const myButton = document.querySelector("button");
 const myButton = document.querySelector("#submitBtn");
+const delButton = docuent.querySelector("#deleteBtn");
 //const myButton = document.getElementById("submitBtn");
 // there is also document.getElementsByClassName
 //document.getElementsByTagName
@@ -65,10 +50,16 @@ function onButtonClick() {
     let red = Math.floor(Math.random() * 256);
     document.body.style.backgroundColor = `rgb(${red}, 128, 128)`;
 }
-
+function onDelete(){
+console.log("On delete");
+let element = resultsCont;
+while (element.firstChild){
+    element.removeChild(element.firstChild);
+}
+}
 //can only have one click
 myButton.click = onButtonClick;
+delButton.onclick = onDelete;
 //can have many additional events handled by addEventListener
 // myButton.addEventListener("click", onButtonClick);
-
 // document.addEventListener("click", () => console.log("Whole Doc clicked"));
