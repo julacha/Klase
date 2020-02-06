7893ede8d1629337666ab8e68635dcaff8a5f313
Julia.Zorina <julia.zorinaa@gmail.com>
2020-02-06 12:26:30 +0200

    add changes

--- scripts/avg.js
+++ scripts/avg.js
@@ -1,5 +1,18 @@
 ///Пример: Случайное целое между min и max
 //использование Math.round() даст неравномерное распределение!
+console.log("Starting work", 6 ** 2);
+let a = 1000; //global a
+//giving a default value to function argument
+function printStuff(a = 50) {
+    console.log("Trying to print a", a);
+    let b = a + 5;
+    let c = a + 6;
+
+    console.log(`Hello Valdis ${a * 82} and there more to know about ${b}`);
+    console.log(`Hello Liga`, b * 2);
+    console.log(`Hello Peter Mr.` + c + " " + b);
+}
+
 function randomRange(myMin, myMax) {
     return Math.floor(Math.random() * (myMax - myMin + 1)) + myMin; // Change this line
 }
@@ -20,24 +33,42 @@ function getAverage(numThrows) {
 const numThrows = 100000;
 let result = getAverage(numThrows);
 
-// 4 ways of selecting a single button
-//
+console.log("Going to print answer");
 
 let myParagraph = document.querySelector("#answer");
 // let myParagraph = document.querySelector("p");
 myParagraph.myResult = result;
 myParagraph.textContent = `Average throw value is ${myParagraph.myResult}`;
 
-const myButton = documment.querySelector(".pretty-button");
+//Many ways of selecting a single buton
+// const myButton = document.querySelector(".pretty-button");
+// const myButton = document.querySelector("button");
+const myButton = document.querySelector("#submitBtn");
+//const myButton = document.getElementById("submitBtn");
+// there is also document.getElementsByClassName
+//document.getElementsByTagName
+
+myButton.style.backgroundColor = "orange";
+//preferable to add a class instead of doing inline
 myButton.classList.add("big-button");
 
 const myInput = document.querySelector(".num-input");
 console.log(`My value is ${myInput.value}`);
 
-function onButtonClick(){
-console.log ("Button was clicked");
-console.log ()
+function onButtonClick() {
+    console.log("Button was clicked!");
+    console.log(`My value is ${myInput.value}`);
+    let nThrows = myInput.value;
+    let result = getAverage(nThrows);
+    myParagraph.textContent = `Average throw value is ${result}`;
+
+    let red = Math.floor(Math.random() * 256);
+    document.body.style.backgroundColor = `rgb(${red}, 128, 128)`;
 }
 
-myButton.addEventListener('click',() => console.log ("Vliked me!"));
-//document.addEventListener ("click",() => console.log ("Whole Doc clicked"));
+//can only have one click
+myButton.click = onButtonClick;
+//can have many additional events handled by addEventListener
+// myButton.addEventListener("click", onButtonClick);
+
+// document.addEventListener("click", () => console.log("Whole Doc clicked"));
