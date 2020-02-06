845338bd0ce0e74fde7ec7c88d04c6d3030a55dc
Julia.Zorina <julia.zorinaa@gmail.com>
2020-02-06 10:53:59 +0200

    add changes

--- scripts/avg.js
+++ scripts/avg.js
@@ -4,9 +4,8 @@ function randomRange(myMin, myMax) {
     return Math.floor(Math.random() * (myMax - myMin + 1)) + myMin;
   } 
 function getAverage(numThrows) {
-    //write function to get an avarage value from dice throws
 let total = 0;
-for (let i=0; i < numThrows; i++){
+for (let i=0; i < numThrows; i++) {
   total +-randomRange(1,6); 
 }
  return total / numThrows;
