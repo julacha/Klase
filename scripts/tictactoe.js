6c942a724449860d8cbfe5a5cc75a4d9785551e7
Julia.Zorina <julia.zorinaa@gmail.com>
2020-02-07 13:07:36 +0200

    added start 2

--- scripts/tictactoe.js
+++ scripts/tictactoe.js
@@ -2,12 +2,19 @@ console.log ("Started Tic Tac Toe");
 //const squares = document.querySelectorAll("square")
 const squares = document.getElementsByClassName("square");
 
+function onClick(event) {
+    console.log("Clicked some button:");
+    console.log ("y id is", event.current);
+}
+
 function addListeners(){
     console.log ("Adding Listeners");
     console.log(squares.length);
+
     for (let i = 0; i < squares.length; i++){
     const sq = squares [i];
-    console.log("My id is, sq.id");
+    console.log("My id is",sq.id);
+    sq.addEventListener ("click", ()=>console.log ("Clicked me!"));
 }
 }
 
@@ -17,7 +24,3 @@ addListeners();
 }
 
 main ();
-/* function onClick() {
-    console.log("Clicked on button:");
-}
- */
